9b38e7543810ddeeb941a5390237ffe372cafd74
Kluska90 <gkluz@gmail.com>
2022-11-22 00:33:58 +0000

    calendar

--- app/src/main/java/com/example/forfoodiesbyfoodies/DetailsView.java
+++ app/src/main/java/com/example/forfoodiesbyfoodies/DetailsView.java
@@ -2,14 +2,27 @@ package com.example.forfoodiesbyfoodies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DetailsView extends AppCompatActivity {
+
+    Button bookking;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details_view);
 
+        bookking= findViewById(R.id.make_booking);
+
+        bookking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DetailsView.this,MakeBooking.class));
+            }
+        });
     }
 }
